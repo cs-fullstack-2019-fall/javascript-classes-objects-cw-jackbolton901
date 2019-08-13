d61d886a5cd17f2adf7bf82513f21c0a40072340
jackbolton901 <bltnjck@gmail.com>
2019-08-13 16:53:03 -0500

    classwork, using classes

---
+++ index.js
@@ -0,0 +1,28 @@
+class Repository
+{
+    constructor(userName,fileName, descripOfRep, code)
+    {
+        this.usernamestr= userName;
+        this.filenamestr= fileName;
+        this.descripstr= descripOfRep;
+        this.codestr= code;
+    }
+    printProps(){
+        console.log(this.usernamestr);
+        console.log(this.filenamestr);
+        console.log(this.descripstr);
+        console.log(this.codestr);
+}
+
+}
+let myRepository = new Repository("jackbolton901","JavaScript", "Classwork","While loops");
+myRepository.printProps()
+
+
+// Problem 2
+ class Movies
+ {
+    constructor(movieName, rating, releaseYear,);
+
+
+ }
